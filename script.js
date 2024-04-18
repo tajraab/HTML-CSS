1a2c1db0bcd0b57260bec611d15721f0f4b37c11
Tajra <tajrabacevac@gmail.com>
2024-04-18 13:49:54 +0200

    .

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 
-let js = "amazing"
-      if (js === "amazing") alert("java script is valid")
-      40 + 28 + 23
-console.log(40+28+30);
+// let js = "amazing"
+//       if (js === "amazing") alert("java script is valid")
+//       40 + 28 + 23
+// console.log(40+28+30);
+// let firstName="Tajra";
+// console.log(firstName);
+const now=2037;
+const ageJonas=now-1991;
+const ageSara=now-2018;
+console.log(ageJonas,ageSara);
+console.log(ageJonas*2,ageJonas/10,2**3)
