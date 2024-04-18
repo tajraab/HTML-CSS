87b044ef0c792ec2b177d2686ced9f6874e92521
Tajra <tajrabacevac@gmail.com>
2024-04-18 14:39:06 +0200

    .

--- script.js
+++ script.js
@@ -5,13 +5,23 @@
 // console.log(40+28+30);
 // let firstName="Tajra";
 // console.log(firstName);
-const now=2037;
-const ageJonas=now-1991;
-const ageSara=now-2018;
-console.log(ageJonas,ageSara);
-console.log(ageJonas*2,ageJonas/10,2**3)
+// const now=2037;
+// const ageJonas=now-1991;
+// const ageSara=now-2018;
+// console.log(ageJonas,ageSara);
+// console.log(ageJonas*2,ageJonas/10,2**3)
 
-const firstName="Tajra";
-const lastName="Bacevac";
-console.log(firstName+" "+lastName);
+// const firstName="Tajra";
+// const lastName="Bacevac";
+// console.log(firstName+" "+lastName);
 
+// let x=10+5;
+// console.log(x)
+
+///STRINGS
+const firstName="Jonas";
+const job="teacher";
+const birthYear=1991;
+const year=2037;
+const jonas= "I am" + firstName + "a" + (year-birthYear)+"years old" + job +".";
+console.log(jonas);
